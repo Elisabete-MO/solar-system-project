80d5e07be822caac09f3668be7a0bcc2439b6d92
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-09-26 15:02:58 -0300

    Quinto requisito

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <PlanetCard />
       </>
     );
   }
