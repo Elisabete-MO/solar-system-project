effad99d650fc7a2e2c32d8311329d5c87565079
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-09-26 17:28:44 -0300

    Ajuste arquivo Solar System

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import PlanetCard from './PlanetCard';
 import planets from '../data/planets';
+import Title from './Title';
+
+const headline = 'Planetas';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
+        <Title headline={ headline } />
         {planets
           .map((planeta) => (
             <PlanetCard
