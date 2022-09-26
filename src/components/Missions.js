66d467e58366959f310a573242d01f1a44e86431
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-09-26 17:11:52 -0300

    Oitavo requisito

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import Title from './Title';
+
+const headline = 'Missões';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <Title headline={ headline } />
+      </div>
     );
   }
 }
+
+// Missions.defaultProps = {
+//   headline: ' ',
+// };
+
+// Missions.propTypes = {
+//   headline: PropTypes.string,
+// };
 
 export default Missions;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import Title from './Title';
 import PlanetCard from './PlanetCard';
 import planets from '../data/planets';
-
-const headline = 'Planetas';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline={ headline } />
         {planets
           .map((planeta) => (
             <PlanetCard
@@ -24,12 +20,10 @@ class SolarSystem extends Component {
 }
 
 // SolarSystem.defaultProps = {
-//   // headline: ' ',
 //   planets: [],
 // };
 
 // SolarSystem.propTypes = {
-//   // headline: PropTypes.string,
 //   planets: PropTypes.arrayOf(PropTypes.shape({
 //     name: PropTypes.string,
 //     image: PropTypes.string,
